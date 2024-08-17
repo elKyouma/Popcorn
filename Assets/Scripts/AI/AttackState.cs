0264979a415e008c47345c2cefc255c6355abd59
Jakub Szydełko <kubarroo248@interia.pl>
2024-08-17 16:06:24 +0200

    Enemy state machine

---
+++ Assets/Scripts/AI/AttackState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+public class AttackState : State
+{
+    public AttackState(Enemy enemy) : base(enemy) { }
+
+    public override State PlayState()
+    {
+        float distanceToTarget = Vector2.Distance(enemyTransform.position, target.position);
+        enemy.SetMoveDirection(Vector2.zero);
+
+        Vector3 direction = (target.position - enemyTransform.position).normalized;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        enemyRb.rotation = 360 - angle;
+
+        if (distanceToTarget > weaponsRange)
+            return new ChaseState(enemy);
+
+        return this;
+    }
+}
