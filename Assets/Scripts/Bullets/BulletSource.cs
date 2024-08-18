bf89a73eddaf5023f65fc451fe012fd704693e75
kapiw04 <wojtowicz.kacper@gmail.com>
2024-08-18 16:00:38 +0200

    Fixed shooting

--- Assets/Scripts/Bullets/BulletSource.cs
+++ Assets/Scripts/Bullets/BulletSource.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 /*
@@ -8,65 +9,77 @@ using UnityEngine;
 
 public class BulletSource : MonoBehaviour
 {
-    [SerializeField] private GameObject spiritPrefab;
-    [SerializeField] private float bulletSpeed;
+    [SerializeField] private GameObject bulletPrefab;
+    public float bulletSpeed;
+    public float maxRange;
     [SerializeField] private float minShootDelay;
-    [SerializeField] private float maxRange;
+    [SerializeField] private int poolSize;
     [SerializeField] private LayerMask layer;
+    [Tooltip("How many raycasts are sent in front of the enemy")]
+    [SerializeField] private int visionAccuracy;
+    [Tooltip("How many degrees are between each raycast")]
+    [SerializeField] private float visionAngle;
     private Ray2D ray;
-    private BulletPool bulletPool;
+    private RaycastHit2D hit;
+    private Dictionary<int, Queue<GameObject>> poolDictionary = new Dictionary<int, Queue<GameObject>>();
 
     private void Start()
     {
-        bulletPool = GetComponent<BulletPool>();
-        bulletPool.CreatePool(spiritPrefab, 5);
+        CreatePool(bulletPrefab, poolSize);
         StartCoroutine(Shoot());
     }
+    public void CreatePool(GameObject prefab, int poolSize)
+    {
+        int poolKey = prefab.GetInstanceID();
 
+        if (!poolDictionary.ContainsKey(poolKey))
+        {
+            poolDictionary.Add(poolKey, new Queue<GameObject>());
+
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject newObject = Instantiate(prefab);
+                prefab.GetComponent<Bullet>().bulletSource = this;
+                newObject.SetActive(false);
+                poolDictionary[poolKey].Enqueue(newObject);
+            }
+        }
+    }
+
+    public Bullet ReuseObject(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        int poolKey = prefab.GetInstanceID();
+
+        if (poolDictionary.ContainsKey(poolKey) && poolDictionary[poolKey].Count <= 0)
+            return null;
+
+        GameObject objectToReuse = poolDictionary[poolKey].Dequeue();
+        poolDictionary[poolKey].Enqueue(objectToReuse);
+
+        objectToReuse.SetActive(true);
+        objectToReuse.transform.position = position;
+        objectToReuse.transform.rotation = rotation;
+
+        return objectToReuse.GetComponent<Bullet>();
+    }
+    private void FixedUpdate()
+    {
+        // ray = new Ray2D(transform.position, transform.right);
+        for (int i = 0; i < visionAccuracy; i++)
+        {
+            ray = new Ray2D(transform.position, Quaternion.Euler(0, 0, -visionAngle / 2 + i * visionAngle / (visionAccuracy - 1)) * transform.right);
+            Debug.DrawRay(ray.origin, ray.direction * maxRange, Color.red, 0.5f);
+            hit = Physics2D.CircleCast(ray.origin, 0.25f, ray.direction, maxRange, layer);
+        }
+    }
     IEnumerator Shoot()
     {
         while (true)
         {
+            Bullet bullet = ReuseObject(bulletPrefab, transform.position, transform.rotation);
+            bullet.FireBullet();
+
             yield return new WaitForSeconds(minShootDelay);
-            FireBullet();
         }
-    }
-
-    private void FireBullet()
-    {
-        ray = new Ray2D(transform.position, transform.right);
-
-        RaycastHit2D hit = Physics2D.CircleCast(ray.origin, 0.25f, ray.direction, maxRange, layer);
-
-        GameObject spirit = bulletPool.ReuseObject(spiritPrefab, ray.origin, Quaternion.identity);
-        // Shooting only when some target can be hit
-        if (hit.collider != null)
-        {
-            Debug.Log(hit.collider.name);
-            StartCoroutine(MoveSpirit(spirit, ray.origin, hit.point));
-        }
-        // else
-        // {
-        //     StartCoroutine(MoveSpirit(spirit, ray.origin, ray.origin + ray.direction * maxRange));
-        // }
-    }
-
-    IEnumerator MoveSpirit(GameObject spirit, Vector2 start, Vector2 end)
-    {
-        float distance = Vector2.Distance(start, end);
-        float time = distance / bulletSpeed;
-        float elapsedTime = 0;
-
-        while (elapsedTime < time)
-        {
-            spirit.transform.position = Vector2.Lerp(start, end, elapsedTime / time);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        spirit.transform.position = end;
-        spirit.GetComponent<SpriteRenderer>().sprite = bulletPool.explosionSprite;
-        yield return new WaitForSeconds(0.1f);
-        spirit.SetActive(false);
     }
 }
