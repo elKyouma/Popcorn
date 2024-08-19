ad10ac44d73bc1ca88c08153c3ce4db95020f87f
kapiw04 <wojtowicz.kacper@gmail.com>
2024-08-19 19:21:26 +0200

    Camera zoom adjusted based on velocity

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -2,48 +2,62 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour {
-    Dictionary<string, float> zoomLevels = new Dictionary<string, float> {
-        {"SmallShip", 0.5f},
-        {"NormalShip", 1f},
-        {"BigShip", 2f},
-        {"HugeShip", 4f}
+public class CameraManager : MonoBehaviour
+{
+    enum ZoomLevels
+    {
+        SmallShip,
+        NormalShip,
+        BigShip,
+        HugeShip
+    }
+    [SerializeField] private ZoomLevels currentZoomLevel = ZoomLevels.NormalShip;
+    Dictionary<ZoomLevels, float> zoomLevels = new Dictionary<ZoomLevels, float> {
+        { ZoomLevels.SmallShip, 0.5f },
+        { ZoomLevels.NormalShip, 1.0f },
+        { ZoomLevels.BigShip, 1.5f },
+        { ZoomLevels.HugeShip, 2.0f }
     };
 
-
-    [SerializeField]
+    private Transform target;
     private const float defaultZoom = 5.0f;
-    CinemachineVirtualCamera virtualCamera;
+    private CinemachineVirtualCamera virtualCamera;
 
-    private void Start() {
+    [Header("Zoom on Velocity")]
+    [Tooltip("Maximum FOV when ship is at speed threshold")]
+    [SerializeField] private float maxFOV = 100;
+    [Tooltip("At what speed should the camera be at max FOV")]
+    [SerializeField] private float speedThreshold = 10;
+    private float extraZoom = 0;
+
+    private void Start()
+    {
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        target = virtualCamera.Follow;
     }
 
-    // temporary solution
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            SetCamZoomMultiplier(zoomLevels["SmallShip"]);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            SetCamZoomMultiplier(zoomLevels["NormalShip"]);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            SetCamZoomMultiplier(zoomLevels["BigShip"]);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4)) {
-            SetCamZoomMultiplier(zoomLevels["HugeShip"]);
-        }
+    private void Update()
+    {
+        AdjustExtraZoom();
+        SetCamZoomMultiplier(zoomLevels[currentZoomLevel]);
     }
 
-    public void SetCamZoomMultiplier(float zoom) {
+    public void AdjustExtraZoom()
+    {
+        extraZoom = this.target.GetComponent<Rigidbody2D>().velocity.magnitude > speedThreshold ? maxFOV : 0;
+    }
+
+    public void SetCamZoomMultiplier(float zoom)
+    {
         float current = virtualCamera.m_Lens.OrthographicSize;
-        float target = defaultZoom * zoom;
-        Debug.Log("Current: " + current + " Target: " + target);
+        float target = defaultZoom * zoom + extraZoom;
+        Debug.Log("Speed: " + this.target.GetComponent<Rigidbody2D>().velocity.magnitude);
 
-        // Smoothly interpolate between the current size and the target size.
-        LeanTween.value(gameObject, current, target, 0.5f).setOnUpdate((float val) => {
+        if (current == target) return;
+
+        LeanTween.value(gameObject, current, target, 0.5f).setOnUpdate((float val) =>
+        {
             virtualCamera.m_Lens.OrthographicSize = val;
         });
     }
-
 }
