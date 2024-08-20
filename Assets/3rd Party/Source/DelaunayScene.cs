0fdc3b0d7fef7a903ae4be4e2714f303c3b743c8
Dawid Hebda <dawhebda@gmail.com>
2024-08-20 07:18:07 +0200

    WorldManager can generate meshes for map

--- Assets/3rd Party/Source/DelaunayScene.cs
+++ Assets/3rd Party/Source/DelaunayScene.cs
@@ -7,65 +7,14 @@ using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
-namespace Source {
+namespace Source
+{
 
     public class DelaunayScene : MonoBehaviour
     {
-        private MeshFilter meshFilter;
-        private Mesh mesh;
-        private int testIndex = 0;
-        private TestShape gameMap = new TestShape();
-
-        private void Awake() {
-            Application.targetFrameRate = 60;
-            this.meshFilter = gameObject.GetComponent<MeshFilter>();
-            this.mesh = new Mesh();
-            this.mesh.MarkDynamic();
-            this.meshFilter.mesh = mesh;
-        }
         public void Next()
         {
-            gameMap.hull = WorldManager.mapOutlinePoints.ToArray();
-            gameMap.holes = new Vector2[WorldManager.planetsOutlines.Count][];
-            int i = 0;
-            foreach (List<Vector2> list in WorldManager.planetsOutlines)
-            {
-                print(list.Count);
-                gameMap.holes[i] = list.ToArray();
-                i++;
-            }
-            print(gameMap.hull);
-            print(gameMap.holes);
-            setTest();
-        }
-
-
-        private void setTest() {
-            var iGeom = IntGeom.DefGeom;
-
-            var pShape = gameMap.ToPlainShape(iGeom, Allocator.Temp);
-            //Here Error \/
-            print(pShape);
-            var triangles = pShape.DelaunayTriangulate(Allocator.Temp);
-            var points = iGeom.Float(pShape.points, Allocator.Temp);
-            var vertices = new NativeArray<float3>(points.Length, Allocator.Temp);
-            for (int i = 0; i < points.Length; ++i)
-            {
-                var p = points[i];
-                vertices[i] = new float3(p.x, p.y, 0);
-            }
-            points.Dispose();
-
-            var bodyMesh = new StaticPrimitiveMesh(vertices, triangles);
-            vertices.Dispose();
-            triangles.Dispose();
-
-            var colorMesh = new NativeColorMesh(vertices.Length, Allocator.Temp);
-
-            colorMesh.AddAndDispose(bodyMesh, Color.green);
-            colorMesh.FillAndDispose(mesh);
-
-            pShape.Dispose();
+            WorldManager.Get();
         }
     }
 
