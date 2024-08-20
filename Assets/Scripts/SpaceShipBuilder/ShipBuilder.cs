b371acd377ecd325468be8a95f71ec55d1da323f
elKyouma <47083340+elKyouma@users.noreply.github.com>
2024-08-20 16:07:25 +0200

    rzeczy

--- Assets/Scripts/SpaceShipBuilder/ShipBuilder.cs
+++ Assets/Scripts/SpaceShipBuilder/ShipBuilder.cs
@@ -187,6 +187,9 @@ public class ShipBuilder : MonoBehaviour
 
     public void DestroyElement(Vector2Int vec)
     {
+        if (vec == Vector2Int.zero)
+            elements[Vector2Int.zero].TakeDamage(10000);
+
         if (!elements.ContainsKey(vec)) return;
         Instantiate(destructionParticles, elements[vec].transform.position, Quaternion.identity);
         Destroy(elements[vec].gameObject);
