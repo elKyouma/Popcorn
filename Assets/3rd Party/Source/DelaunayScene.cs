f23692eb41b832b906325c2d82497ac3ba8cfdd4
Dawid Hebda <dawhebda@gmail.com>
2024-08-20 02:38:45 +0200

    Saved private scene

--- Assets/3rd Party/Source/DelaunayScene.cs
+++ Assets/3rd Party/Source/DelaunayScene.cs
@@ -1,6 +1,7 @@
 using iShape.Geometry;
 using iShape.Mesh2d;
 using iShape.Triangulation.Shape.Delaunay;
+using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
@@ -12,6 +13,7 @@ namespace Source {
         private MeshFilter meshFilter;
         private Mesh mesh;
         private int testIndex = 0;
+        private TestShape gameMap = new TestShape();
 
         private void Awake() {
             Application.targetFrameRate = 60;
@@ -19,39 +21,45 @@ namespace Source {
             this.mesh = new Mesh();
             this.mesh.MarkDynamic();
             this.meshFilter.mesh = mesh;
-            setTest(testIndex);
+            setTest();
         }
-
         public void Next() {
-            testIndex = (testIndex + 1) % TriangulationTests.Data.Length;
-            setTest(testIndex);
+            gameMap.hull = WorldManager.mapOutlinePoints.ToArray();
+            gameMap.holes = new Vector2[WorldManager.planetsOutlines.Count][];
+            int i = 0;
+            foreach (List<Vector2> list in WorldManager.planetsOutlines)
+            {
+                gameMap.holes[i] = list.ToArray();
+                i++;
+            }
+            setTest();
         }
 
-        private void setTest(int index) {
+        private void setTest() {
             var iGeom = IntGeom.DefGeom;
 
-            var pShape = TriangulationTests.Data[index].ToPlainShape(iGeom, Allocator.Temp);
-            var triangles = pShape.DelaunayTriangulate(Allocator.Temp);
+            var pShape = gameMap.ToPlainShape(iGeom, Allocator.Temp);
+            //Here Error \/
+            //var triangles = pShape.DelaunayTriangulate(Allocator.Temp);
+            //var points = iGeom.Float(pShape.points, Allocator.Temp);
+            //var vertices = new NativeArray<float3>(points.Length, Allocator.Temp);
+            //for (int i = 0; i < points.Length; ++i) {
+            //    var p = points[i];
+            //    vertices[i] = new float3(p.x, p.y, 0);
+            //}
+            //points.Dispose();
+            
+            //var bodyMesh = new StaticPrimitiveMesh(vertices, triangles);
+            
+            //vertices.Dispose();
+            //triangles.Dispose();
+            
+            //var colorMesh = new NativeColorMesh(vertices.Length, Allocator.Temp);
+            
+            //colorMesh.AddAndDispose(bodyMesh, Color.green);
+            //colorMesh.FillAndDispose(mesh);
 
-            var points = iGeom.Float(pShape.points, Allocator.Temp);
-            var vertices = new NativeArray<float3>(points.Length, Allocator.Temp);
-            for (int i = 0; i < points.Length; ++i) {
-                var p = points[i];
-                vertices[i] = new float3(p.x, p.y, 0);
-            }
-            points.Dispose();
-            
-            var bodyMesh = new StaticPrimitiveMesh(vertices, triangles);
-            
-            vertices.Dispose();
-            triangles.Dispose();
-            
-            var colorMesh = new NativeColorMesh(vertices.Length, Allocator.Temp);
-            
-            colorMesh.AddAndDispose(bodyMesh, Color.green);
-            colorMesh.FillAndDispose(mesh);
-
-            pShape.Dispose();
+            //pShape.Dispose();
         }
     }
 
